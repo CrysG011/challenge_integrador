b5f8e4dc752dcd258a0b97e5d798b807f5676949
CrystinaG <crystinagonz@gmail.com>
2023-08-26 15:01:27 -0300

    SHOP estructura para componentes

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ function loadComponent(url, containerId) {
 }
 
 // Cargar header.html en el div con id "header"
-loadComponent("components/header.html", "header");
+loadComponent("/components/header.html", "header");
 
 // Cargar footer.html en el div con id "footer"
-loadComponent("components/footer.html", "footer");
+loadComponent("/components/footer.html", "footer");
+
+// Seleccionar todos los contenedores de noticias por clase y cargar el contenido
+const newsContainers = document.querySelectorAll('.news-container');
+newsContainers.forEach(container => {
+    loadComponent("/components/news.html", container.id);
+});
